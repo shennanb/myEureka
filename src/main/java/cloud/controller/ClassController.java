186c1bdec42548c4f285deba8313f53363da8508
shennanb <378191876@qq.com>
2019-09-02 10:56:05 +0800

    修改4

--- src/main/java/cloud/controller/ClassController.java
+++ src/main/java/cloud/controller/ClassController.java
@@ -10,7 +10,7 @@ public class ClassController {
 
     @GetMapping("/classes")
     public Object hello(@RequestParam String name) {
-        return "get3";
+        return "get4";
     }
 
     @PostMapping("/classes")
